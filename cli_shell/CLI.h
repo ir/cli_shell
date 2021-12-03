54ec0922589c378c385e05845fe802cdf9362fa3
ir <71415296+ir@users.noreply.github.com>
2021-12-02 22:10:49 -0900

    Add project files.

---
+++ cli_shell/CLI.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef __CLI
+#include <iostream>
+#include <vector>
+#include <string>
+
+#endif // !__CLI
+
+class CLI
+{
+public:
+	CLI();
+	~CLI();
+
+private:
+
+};
+
+CLI::CLI()
+{
+}
+
+CLI::~CLI()
+{
+}
+
